a5234c3763880dcbaebc92608414f7dea800abe6
dm274516 <daniel-alfonso.montoyavasquez@cea.fr>
2023-12-04 13:40:26 +0100

    Removed old mahalanobis distance functions

--- ls_ood_detect_cea/__init__.py
+++ ls_ood_detect_cea/__init__.py
@@ -3,6 +3,5 @@ from .detectors import *
 from .dimensionality_reduction import *
 from .metrics import *
 from .ood_detection_dataset import *
-from .distance import *
 from .score import *
 from .rcnn import *
